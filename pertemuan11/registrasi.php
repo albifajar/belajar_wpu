da9e4f839d001b65e659f0c9d7df3fe9bff9ee88
alfarama <albi@sawala.tech>
2022-06-05 19:38:45 +0700

    feat: register sistem

--- pertemuan11/registrasi.php
+++ pertemuan11/registrasi.php
@@ -1,36 +1,45 @@
 <?php
 
 require "functions.php";
+if( isset($_POST["register"]) ) {
+	if( register($_POST) > 0 ) {
+		echo "<script>
+				alert('user baru berhasil ditambahkan, silahkan login!');
+				document.location.href = 'login.php';
+			  </script>";
+	} else {
+		echo mysqli_error($connection);
+	}
+}
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     
-    <title>Halaman Registrasi</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Registrasi</title>
+    <link rel="stylesheet" href="./style.css?v=1">
   </head>
   <body>
 	  <main>
       <h1>Registrasi</h1>
-      
 	      <form method="post" action="" enctype="multipart/form-data">
-				<ul class="input-list">
-					<li>
-						<label for="username">Username</label>
-						<input type="text" name="username" id="username">
-					</li>
-					<li>
-						<label for="password">Password</label>
-						<input type="password" name="password" id="password">
-					</li>
-					<li>
-						<label for="konfpass">Konfirmasi Password</label>
-						<input type="password" name="konfpass" id="konfpass">
-					</li>
-					<li>
-						<button type="submit" name="submit" class="green">Daftar!</button>
-					</li>
-				</ul>
+			<ul class="input-list">
+				<li>
+					<label for="username">Username</label>
+					<input type="text" name="username" id="username">
+				</li>
+				<li>
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password">
+				</li>
+				<li>
+					<label for="confpass">Konfirmasi Password</label>
+					<input type="password" name="confpass" id="confpass">
+				</li>
+				<li>
+					<button type="submit" name="register" class="green">Daftar!</button>
+				</li>
+			</ul>
       </form>
 	  <main>
   </body>
